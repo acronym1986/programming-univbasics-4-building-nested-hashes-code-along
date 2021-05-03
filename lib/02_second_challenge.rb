e37131454c05bb302a4df911f1d605b041cbb806
Tim <acronym1986@gmail.com>
2021-05-04 09:52:36 +1000

    done

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,10 +1,13 @@
 def second_challenge
+  #:patriarch, :matriarch, :hero, :hero_friends
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch=>{}, :matriarch=>{}, :hero=>{}, :hero_friends=>[]
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch=>{}, :matriarch=>{}, :heroine=>{}, :heroine_friends=>[]
       }}
 
   
